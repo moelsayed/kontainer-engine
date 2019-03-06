a06ec994d8e1f2e8b4a2fe74b20a74a3a3771374
moelsayed <m.elsayed@gmail.com>
2019-03-06 02:43:44 +0200

    wip

--- cluster/backup.go
+++ cluster/backup.go
@@ -2,6 +2,7 @@ package cluster
 
 import (
 	"context"
+	"encoding/json"
 
 	"github.com/rancher/kontainer-engine/types"
 )
@@ -15,11 +16,40 @@ func (c *Cluster) ETCDSave(ctx context.Context, snapshotName string) error {
 }
 
 func (c *Cluster) ETCDRestore(ctx context.Context, snapshotName string) error {
-	driverOpts, err := c.getDriverOps()
+	if err := c.restore(); err != nil {
+		return err
+	}
+	driverOpts, err := c.ConfigGetter.GetConfig()
 	if err != nil {
 		return err
 	}
-	return c.Driver.ETCDRestore(ctx, toInfo(c), driverOpts, snapshotName)
+	driverOpts.StringOptions["name"] = c.Name
+
+	for k, v := range c.Metadata {
+		if k == "state" {
+			state := make(map[string]interface{})
+			if err := json.Unmarshal([]byte(v), &state); err == nil {
+				flattenIfNotExist(state, &driverOpts)
+			}
+
+			continue
+		}
+
+		driverOpts.StringOptions[k] = v
+	}
+
+	if err := c.PersistStore.PersistStatus(*c, Updating); err != nil {
+		return err
+	}
+
+	info, err := c.Driver.ETCDRestore(ctx, toInfo(c), &driverOpts, snapshotName)
+	if err != nil {
+		return err
+	}
+
+	transformClusterInfo(c, info)
+
+	return c.PostCheck(ctx)
 }
 
 func (c *Cluster) getDriverOps() (*types.DriverOptions, error) {
